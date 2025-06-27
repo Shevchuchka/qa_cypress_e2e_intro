cef542d1f0617b3045482e1377dcadcf5c30de72
Yuliia <shevchuuchkaaa@gmail.com>
2025-06-27 22:09:07 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,28 @@
 /// <reference types="cypress" />
 
+const username = 'username';
+const email = 'useremail@gmail.com';
+const password = 'password1234';
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('/');
+    cy.contains('a', 'Sign in')
+      .should('exist').click();
 
+    cy.url().should('include', '/login');
+    cy.get('h1').should('contain.text', 'Sign in');
+
+    cy.get('[placeholder=Email]')
+      .type(`${email}`);
+
+    cy.get('[placeholder=Password]')
+      .type(`${password}`);
+
+    cy.contains('button', 'Sign in')
+      .should('exist').click();
+
+    cy.contains('a', `${username}`)
+      .should('exist');
   });
 });
